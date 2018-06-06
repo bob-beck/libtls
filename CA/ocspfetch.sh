cdb5aa32f73ef707d8a1ba900575f070eabccb97
Bob Beck <beck@openbsd.org>
2018-06-06 14:11:28 -0600

    shut up openssl ickyness, we only need the der

--- CA/ocspfetch.sh
+++ CA/ocspfetch.sh
@@ -7,8 +7,8 @@ out=server-ocsp.der
 serial=`openssl x509 -noout -serial -in ${cert} | sed 's/serial=//'`
 ocsp_uri=`openssl x509 -noout -ocsp_uri -in ${cert}`
 ocsp_host=`echo ${ocsp_uri} | sed '-e s#http://##' -e 's#[:/].*##'`
-openssl ocsp -out /dev/null -no_nonce -issuer ${chain} -VAfile ${chain} -header Host ${ocsp_host} -url ${ocsp_uri} -serial 0x${serial} -respout ${out}.new
-openssl ocsp -out /dev/null -no_nonce -VAfile ${chain} -issuer ${chain} -serial 0x${serial} -respin ${out}.new
+openssl ocsp -out /dev/null -no_nonce -issuer ${chain} -VAfile ${chain} -header Host ${ocsp_host} -url ${ocsp_uri} -serial 0x${serial} -respout ${out}.new >/dev/null 2>&1
+openssl ocsp -out /dev/null -no_nonce -VAfile ${chain} -issuer ${chain} -serial 0x${serial} -respin ${out}.new >/dev/null 2>&1
 if [ $? == 0 ]; then
     if [ -r "${out}" ]; then
 	mv ${out} ${out}.old
